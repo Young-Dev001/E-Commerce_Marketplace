9792b5bb74cd6943311eefca4f40c32bfbf2643b
YoungDev001 <kips.technologies.msa@gmail.com>
2025-02-28 00:18:10 +0300

    ecommerce_market_place💲💲💲

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -92,10 +92,7 @@ export default function Login({
 
                 <div className="flex items-center justify-end mt-4">
                     {canResetPassword && (
-                        <Link
-                            href={route("password.request")}
-                            className="text-sm text-gray-600 underline rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
-                        >
+                        <Link href={route("password.request")} className="link">
                             Forgot your password?
                         </Link>
                     )}
